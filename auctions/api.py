d31db2388f69128b0d0865db92bdd052ae258313
Victor Valdez <victorvaldez5@gmail.com>
2020-12-15 13:50:13 -0600

    Make it a flask app!

--- auctions/api.py
+++ auctions/api.py
@@ -1,22 +1,12 @@
-import flask
+from wow_client import WowDataClient
+from .controller import AuctionDataController
+from flask import Flask
 import os
 
-from wow_client import WowDataClient
-from auctions.controller import AuctionDataController
-from items.controller import ItemDataTable
-from dotenv import load_dotenv
-from pathlib import Path
-
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
-
-env_path = Path('.env')
-load_dotenv(env_path)
+app = Flask(__name__)
 
 @app.route('/auctions')
 def auctions_api():
     client = WowDataClient(client_id=os.getenv('wow_app_client_id'), client_secret=os.getenv('wow_app_client_secret'), realmId=73)
     auctioneer = AuctionDataController(client=client)
     return auctioneer.auctions.to_dict()
-
-app.run()
